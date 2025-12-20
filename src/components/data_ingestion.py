8e2ac52483564b3771e6fc940cd12ca4f6dfd00a
lakshmi <laks@lakshmis-MacBook-Pro.local>
2025-12-20 17:16:40 -0600

    Added Flask

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngesionConfig:
     train_data_path:str=os.path.join("artifacts","train.csv")
@@ -46,4 +49,6 @@ if __name__=="__main__":
     train_data,test_data,_=obj.initiate_data_ingestion()
     logging.info("Data ingestion ended")
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
